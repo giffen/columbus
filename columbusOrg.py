c2151f8f820c1d8d4a02dcc2e22146d11b9df5ff
amcew050 <andrea.h.mcewan@gmail.com>
2015-03-23 21:09:53 -0400

    added proper headers

--- columbusOrg.py
+++ columbusOrg.py
@@ -1,35 +1,51 @@
 
-import os, csv
+import os, csv, re
 
 path= raw_input("Please enter the file path: ")
+if ' ' in path:
+	print "\nPlease remove any whitespaces from folder and filenames "
+	exit()
+
 os.chdir(str(path))
 
-rows = int(raw_input("\n\nHow many rows would you like to include in your analysis?"))
+rows = int(raw_input("\nHow many rows would you like to include in your analysis? "))
 
-mk_graph = raw_input("\n\nWould you like to make a graph? (Y/N) ")
+mk_graph = raw_input("\nWould you like to make a graph? (Y/N) ")
 
 
 def columbus():
 	filelist = []
-	#num = 1000
 	for (dirpath, dirnames, filenames) in os.walk(path):
 		for f in  filenames:
 			if 'Selected' not in f and 'Population' in f:
 				filelist.append(f)
 	return filelist
 
-def prismFile(files):
+def headers(filelist):
+	header = []
+	for f in filelist:
+		pattern = r'(\.\w\d+)'
+	 	h = re.search(pattern,f)
+	 	if h:
+	 		header += [h.group()]
+	header = [s.replace('.','') for s in header]
+	return header
+
+def prismFile(files, header):
 	container = []
+	headcount = 0
 	for f in files:
-		csvfile = open(f, 'r')
-		reader = csvfile.readlines()
-		data = []
-		container.append(data)
 		count = 0
+		csvfile = open(f, 'r')				
+		reader = csvfile.readlines()[1:]
+		data = []
+		container.append(data)	
+		data.append(header[headcount])	
+		headcount += 1			
 		for line in reader:
 			array = line.split(',')
 			content = array[8]			
-			if count < rows + 1:
+			if count < rows + 1:				
 				data.append(content)
 				count += 1
 	return container
@@ -38,19 +54,20 @@ def write_csv(data):
 	with open('summary_file.csv', 'wb') as fp:
 		a = csv.writer(fp, delimiter=',')
 		a.writerows(data)
-	print "\n\nFinished making a summary file! Check your folder for summary_file.csv"
+	print ("\n\nFinished making a summary file! Check your folder for summary_file.csv")
 
 def graph_summary():
-	print "\n\nMaking graph..."
+	print ("\n\nMaking graph...")
 	import os, pandas as pd, matplotlib.pyplot as plt
 	df = pd.read_csv('summary_file.csv')
 	p = df.plot(kind='box')
-	p.set_ylim(100,5000)
+	p.set_ylim(100,15000)
 	plt.show()
 											
 def main():
 	files = columbus()
-	summary_file = prismFile(files)
+	header = headers(files)
+	summary_file = prismFile(files, header)
 	write_csv (zip(*summary_file))
 	if mk_graph.upper() == 'Y':
 		graph_summary()	
